1264c77baf0a74ea836bc7460210bcb4e0c0d7b4
yasser732boubessit <163558067+yasser732boubessit@users.noreply.github.com>
2025-04-12 15:53:17 +0100

    Delete gestion-items-service directory

--- gestion-items-service/src/main/java/com/example/gestionitems/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.gestionitems.repository;
-
-import com.example.gestionitems.model.Item;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ItemRepository extends MongoRepository<Item, String> {
-}
